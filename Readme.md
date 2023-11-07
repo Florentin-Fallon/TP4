d1c08da449a6ff0521368f4e12b25f8e4bb0a13b
FlorentinFallon <119119355+FlorentinFallon@users.noreply.github.com>
2023-11-07 14:51:45 +0100

    tp4

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# I. Simple bs program
+
+# 1. First steps
+
+# 🌞 bs_server_I1.py
+
+
+
+# 🌞 bs_client_I1.py
+
+
+
+# 🌞 Commandes...
---
+++ bs_client_I1.py
@@ -0,0 +1,26 @@
+from sys import exit
+import socket
+
+host = '192.168.64.37'  # IP du serveur
+port = 13337             # Port choisir par le serveur
+
+# Création de l'objet socket de type TCP (SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connexion au serveur
+s.connect((host, port))
+# note : la double parenthèse n'est pas une erreur : on envoie un tuple à la fonction connect()
+
+# Envoi de data bidon
+s.sendall(b'Meooooo !')
+
+# On reçoit 1024 bytes qui contiennent peut-être une réponse du serveur
+data = s.recv(1024)
+
+# On libère le socket TCP
+s.close()
+
+# Affichage de la réponse reçue du serveur
+print(f"Le serveur a répondu {repr(data)}")
+
+exit(0)
