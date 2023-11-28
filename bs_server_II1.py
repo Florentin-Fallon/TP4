7c75f9439681e826e8244b5f276586062abfc6d5
FlorentinFallon <119119355+FlorentinFallon@users.noreply.github.com>
2023-11-28 09:42:24 +0100

    bs_server_ii1

--- bs_server_II1.py
+++ bs_server_II1.py
@@ -16,6 +16,22 @@ except socket.error:
     print("On dirait qu'il y a eu un soucis, déso.")
     exit(1)
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--port', type=int, help='port number', default=13337)
+parser.add_argument('-h','--help',action='help',default=argparse.SUPPRESS  help='show this help message and exit')
+
+args = parser.parse_args()
+
+if args.port < 0 or args.port > 65535:
+    print("ERROR Le port spécifié n'est pas un port possible (de 0 à 65535).")
+    exit(1)
+elif args.port >= 0 and args.port <= 1024:
+    print("ERROR Le port spécifié est un port privilégié. Spécifiez un port au dessus de 1024.")
+    exit(2)
+else:
+    port = args.port
+
+
 while True:
 
     try:
